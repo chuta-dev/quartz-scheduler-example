846bd9b258b42808a0eb5fc4282c7c694bb7872d
Jesus Salvador RO <jesus.salvador.ro17@gmail.com>
2022-06-10 15:47:23 -0500

    add log4j properties and logger in HelloJob

--- src/main/java/com/chuta/tasks/example/jobs/HelloJob.java
+++ src/main/java/com/chuta/tasks/example/jobs/HelloJob.java
@@ -3,9 +3,16 @@ package com.chuta.tasks.example.jobs;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloJob implements Job {
+    private final Logger logger = LoggerFactory.getLogger(HelloJob.class);
+
     public void execute(JobExecutionContext context)throws JobExecutionException {
         System.out.println("Hello! ┌( ಠ_ಠ)┘");
+
+        logger.info("Hello! ┌( ಠ_ಠ)┘");
+        logger.debug("Hello! ┌( ಠ_ಠ)┘");
     }
 }
